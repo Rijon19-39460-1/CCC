cd32c225c2fa853dfd8bbba0832885dce9ecef5c
Rashedul Haque <rashed.rabby43@gmail.com>
2021-12-09 14:01:09 +0600

    student

---
+++ ConsoleApplication1/ConsoleApplication1/course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class course
+    {
+        private int id;
+        public int Id { get; set; }
+        private string courseName;
+        public String CourseName { get; set; }
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/studentInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class studentInfo
+    {
+        private string name;
+        public String Name { get; set; }
+        private int number;
+        public int Number { get; set; }
+    }
+}
